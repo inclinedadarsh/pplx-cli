9196176e3dba2e7d66a62ffd55981a258a2dc65a
Adarsh Dubey <dubeyadarshmain@gmail.com>
2026-01-30 01:02:50 +0530

    docs: update README with correct search options, bump version to 1.0.1

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { createAskCommand } from './commands/ask.js';
 import { createSearchCommand } from './commands/search.js';
 import { createChatCommand } from './commands/chat.js';
 
-const version = '1.0.0';
+const version = '1.0.1';
 
 program
     .name('pplx')
